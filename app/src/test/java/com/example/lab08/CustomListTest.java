d9855e59a3024ab786e744386182a3b07e764c19
ashleymkang@gmail.com <ashleymkang@gmail.com>
2026-03-03 17:53:22 -0700

    count test failed

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,15 +1,26 @@
 package com.example.lab08;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
-
-    @Test
-    public void testHasCity(){
+    private CustomList setup(){
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
+        return list;
+    }
 
-        assertTrue(list.hasCity(calgary));
+    private City mockCity(){ return new City("Calgary", "AB");}
+
+    @Test
+    public void testHasCity(){
+        CustomList list = setup();
+        assertTrue(list.hasCity(mockCity()));
+    }
+
+    @Test
+    public void testCount(){
+        assertEquals(1, setup().count());
     }
 }
