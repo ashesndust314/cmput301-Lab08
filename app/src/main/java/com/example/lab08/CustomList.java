2986b96a2eee5c219b4d27f8db238f626ffb2f14
ashleymkang@gmail.com <ashleymkang@gmail.com>
2026-03-03 17:54:12 -0700

    count test success

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,4 +18,6 @@ public class CustomList {
 
     // Will be implemented later using TDD (leave blank for now)
     public boolean hasCity(City city) { return cities.contains(city); }
+
+    public int count() {return cities.size(); }
 }
